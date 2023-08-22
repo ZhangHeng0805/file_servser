5dbbd011366a3f1cf114feb5e0ca3dc6b75a4a2e
ZhangHeng0805 <3052847663@qq.com>
2023-08-22 14:26:26 +0800

    系统优化

--- src/main/resources/static/js/myFindFileList.js
+++ src/main/resources/static/js/myFindFileList.js
@@ -11,20 +11,20 @@ function getAllFileType() {
             withCredentials: true
         },
         success: function (d) {
-            var html = "";
-            var html2 = "";
+            var html = [];
+            var html2 = [];
             for (var i = 0; i < d.length; i++) {
                 if (d[i].code == 200) {
-                    html += '<option value="' + d[i].message + '">' + d[i].title + '</option>';
+                    html.push('<option value="' + d[i].message + '">' + d[i].title + '</option>');
                     if (d[i].message != '$all$')
-                        html2 += '<option>' + d[i].title + '</option>';
+                        html2.push('<option>' + d[i].title + '</option>');
                 } else {
                     alert("类型查询错误：" + d[i].message);
                     console.warn("类型查询错误:" + d[i])
                 }
             }
-            $("#type").html(html);
-            $("#upload_path_list").html(html2);
+            $("#type").html(html.join(''));
+            $("#upload_path_list").html(html2.join(''));
             set_select_checked("type", TYPE);
         },
         error: function (e) {
@@ -54,7 +54,7 @@ function sub2() {
     }
     let key = $("#keys").val();
     if (key.length > 0) {
-        if ( getFileList_c % 5 === 0)
+        if (getFileList_c % 5 === 0)
             captcha_model_show("getFileList('" + key + "')");
         else
             getFileList(key);
@@ -83,7 +83,9 @@ function getFileList(key) {
         success: function (d) {
             // console.log(d);
             d = Array.isArray(d) ? d : new Array(d);
-            let isSuccess=isData(d);
+            let isSuccess = isData(d);
+            if (isSuccess || getFileList_c !== 0)
+                getFileList_c++;
             if (isSuccess) {
                 Data = d;
                 TYPE = $("#type").val();
@@ -92,25 +94,25 @@ function getFileList(key) {
                 if (TYPE !== $("#type").val()) ;
                 set_select_checked("type", TYPE);
                 console.warn(d);
-                getFileList_c=0;
+                getFileList_c = 0;
             }
             //处理数据
             handle(d);
             //刷新下拉框数据
             getAllFileType();
             $("#btn_sub2").attr('disabled', false);
-            if (isSuccess||getFileList_c!==0)
-                getFileList_c++;
+
         },
         error: function (e) {
             $("#btn_sub2").attr('disabled', false);
             ajax_error(e);
+            getFileList_c = 0;
         }
     })
 }
 
 function handle(d) {
-    var html = "";
+    var html = [];
     var icon = "static/img/unknown.png";
     var title = "";
     let update_time = "";
@@ -145,40 +147,40 @@ function handle(d) {
             }
             title = msg.obj == null ? "" : msg.obj.name;
             size = msg.obj == null ? "" : getFileSizeFormat(msg.obj.size);
-            html += '<div class="panel panel-success">';
+            html.push('<div class="panel panel-success">');
         } else if ((msg.code + '').startsWith('5')) {
-            html += '<div class="panel panel-danger">';
+            html.push('<div class="panel panel-danger">');
         } else {
-            html += '<div class="panel panel-warning">';
+            html.push('<div class="panel panel-warning">');
         }
-        html += '<div class="panel-heading">' +
-            '<img lsrc="' + icon + '" src="" onerror="loadImg(this)" style="height: 40px;margin-right: 15px;box-shadow:0 0 10px black;">' +
-            '<label style="margin-right: 25px">' + title + '</label>' +
-            '<label class="label label-primary">' + size + '</label>' +
-            '<code>' + update_time + '</code>' +
-            '</div>' +
-            '<div class="panel-body" style="background-color: #afd9ee">' +
-            // '<label class="control-label" for="type">文件路径：</label>' +
-            '    <div class="form-group">' +
-            '       <input class="form-control file_path_' + i + '" type="text" readonly>' +
-            '     </div>';
+        html.push('<div class="panel-heading">');
+        html.push('<img lsrc="' + icon + '" src="" onerror="loadImg(this)" style="height: 40px;margin-right: 15px;box-shadow:0 0 10px black;">');
+        html.push('<label style="margin-right: 25px">' + title + '</label>');
+        html.push('<label class="label label-primary">' + size + '</label>');
+        html.push('<code>' + update_time + '</code>');
+        html.push('</div>');
+        html.push('<div class="panel-body" style="background-color: #afd9ee">');
+        // '<label class="control-label" for="type">文件路径：</label>' +
+        html.push('<div class="form-group">');
+        html.push('<input class="form-control file_path_' + i + '" type="text" readonly>');
+        html.push('</div>');
         if (msg.code == 200) {
-            html += '<div class="form-group">' +
-                '<div class="btn-group"><button data-toggle="dropdown" class="btn btn-search dropdown-toggle btn_' + i + '" ">复制链接<span class="caret"></span></button>' +
-                '<ul role="menu" class="dropdown-menu">' +
-                '<li><a id="c1_' + i + '" onclick="copeText(\'c1_' + i + '\');" title="普通的单个文件直接下载">普通下载地址</a></li>' +
-                '<li><a id="c2_' + i + '" onclick="copeText(\'c2_' + i + '\');" title="断点分片快速下载，需要下载端支持">分片下载地址</a></li>' +
-                '</ul></div>';
-            html += '<div class="btn-group"><a class="btn btn-primary" target="_blank" id="btn' + i + '_download">直接访问</a></div>';
+            html.push('<div class="form-group">');
+            html.push('<div class="btn-group"><button data-toggle="dropdown" class="btn btn-search dropdown-toggle btn_' + i + '" ">复制链接<span class="caret"></span></button>');
+            html.push('<ul role="menu" class="dropdown-menu">');
+            html.push('<li><a id="c1_' + i + '" onclick="copeText(\'c1_' + i + '\');" title="普通的单个文件直接下载">普通下载地址</a></li>');
+            html.push('<li><a id="c2_' + i + '" onclick="copeText(\'c2_' + i + '\');" title="断点分片快速下载，需要下载端支持">分片下载地址</a></li>');
+            html.push('</ul></div>');
+            html.push('<div class="btn-group"><a class="btn btn-primary" target="_blank" id="btn' + i + '_download">直接访问</a></div>');
             auth = msg.obj == null ? "" : msg.obj.auth;
             if (auth != null && auth.length > 0 && auth == "admin") {
-                html += '<div class="btn-group"><a class="btn btn-danger" target="_blank" onclick="del(\'' + msg.message + '\')">删除文件</a></div>';
-                html += '<div class="btn-group"><a class="btn btn-warning" target="_blank" onclick="rename(\'' + msg.message + '\',\'' + title + '\')">文件重命名</a></div>';
+                html.push('<div class="btn-group"><a class="btn btn-danger" target="_blank" onclick="del(\'' + msg.message + '\')">删除文件</a></div>');
+                html.push('<div class="btn-group"><a class="btn btn-warning" target="_blank" onclick="rename(\'' + msg.message + '\',\'' + title + '\')">文件重命名</a></div>');
             }
         }
-        html += '</div></div></div>';
+        html.push('</div></div></div>');
     }
-    $("#files_result").html(html);
+    $("#files_result").html(html.join(""));
     $("#files_num").html('总计:' + d.length + '个文件');
     initData(d);
 }
@@ -231,7 +233,7 @@ function checkCoookie() {
 
 function del(path) {
     if (confirm("确定删除该文件吗？")) {
-        if ( del_file_c % 3 === 0)
+        if (del_file_c % 3 === 0)
             captcha_model_show("del_file('" + path + "')");
         else
             del_file(path);
@@ -262,14 +264,15 @@ function del_file(path) {
                 if (d.code === 200) {
                     getFileList(key);
                 } else {
-                    del_file_c=0;
+                    del_file_c = 0;
                     console.warn(d);
                 }
-                if (d.code === 200||del_file_c!==0)
+                if (d.code === 200 || del_file_c !== 0)
                     del_file_c++;
                 alert(d.message);
             },
             error: function (e) {
+                del_file_c = 0;
                 ajax_error(e);
             }
         })
@@ -279,20 +282,21 @@ function del_file(path) {
 }
 
 function rename(path, oldName) {
-    if ( rename_file_c % 4 === 0)
+    if (rename_file_c % 4 === 0)
         captcha_model_show("rename_file('" + path + "','" + oldName + "')");
     else
         rename_file(path, oldName);
 }
 
 var rename_file_c = 0;
+
 function rename_file(path, oldName) {
     let key = $("#keys").val();
     let code = $("#code").val();
     if (key.length > 0) {
         let renameFile = prompt("文件重命名，请输入新的文件名:", oldName);
         if (renameFile) {
-            if (renameFile !=null && renameFile.length > 0) {
+            if (renameFile != null && renameFile.length > 0) {
                 if (renameFile !== oldName) {
                     $.ajax({
                         url: ZH_URL.rename_file,
@@ -311,14 +315,15 @@ function rename_file(path, oldName) {
                             if (d.code === 200) {
                                 getFileList(key);
                             } else {
-                                rename_file_c=0;
+                                rename_file_c = 0;
                                 console.warn(d);
                             }
-                            if (d.code === 200||rename_file_c!==0)
+                            if (d.code === 200 || rename_file_c !== 0)
                                 rename_file_c++;
                             alert(d.title + '\n' + d.message);
                         },
                         error: function (e) {
+                            rename_file_c = 0;
                             ajax_error(e);
                         }
                     })
